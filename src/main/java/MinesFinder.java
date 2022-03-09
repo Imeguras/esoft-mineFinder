6ae20428ae7550d3a96e861789bdc8b2f3769f62
Imeguras <imeguras@teknik.io>
2022-03-09 10:45:36 +0000

    end of class

--- src/main/java/MinesFinder.java
+++ src/main/java/MinesFinder.java
@@ -3,6 +3,10 @@ import javax.swing.*;
 public class MinesFinder extends JFrame {
     private JPanel painelPrincipal;
     private JLabel Header;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
 
     public MinesFinder(String title) {
         super(title);
